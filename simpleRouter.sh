4c7453703c2f97716b93f11a9eb6b7086f419e64
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2015-11-07 01:01:51 +0900

    awareness of lagopus.dsl in lagopus v0.2.2

--- simpleRouter.sh
+++ simpleRouter.sh
@@ -13,7 +13,7 @@ deploy_simpleRouter() {
     docker run --name $host_name --privileged -h $host_name -v $PWD/$1:/tmp -p $port_num:8080 -itd ttsubo/simple-router /bin/bash
     docker exec $host_name cp /tmp/OpenFlow.ini /root/simpleRouter/rest-client
     docker exec $host_name mkdir /usr/local/etc/lagopus
-    docker exec $host_name cp /tmp/lagopus.conf /usr/local/etc/lagopus
+    docker exec $host_name cp /tmp/lagopus.dsl /usr/local/etc/lagopus
     docker exec $host_name cp /tmp/start_lagopus.sh /root
 }
 
